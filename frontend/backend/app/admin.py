07fb8e07e62a05e1e64c78cc791d8b899299b9a5
koteswaraobalusu <137074795+koteswaraobalusu@users.noreply.github.com>
2025-07-30 12:07:45 -0500

    Delete frontend/backend directory
    delete backend directory

--- frontend/backend/app/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from .models import EmailOTP
-# Register your models here.
-admin.site.register(EmailOTP)
--- frontend/backend/app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import RequestOTPAPIView,RegisterVerifyOTPAPIView
-urlpatterns=[
-
-    path('register/request/',RequestOTPAPIView.as_view(),name='register-request'),
-    path('register/verify/', RegisterVerifyOTPAPIView.as_view(),name='register-verify'),
-    
-]
